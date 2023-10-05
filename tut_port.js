3822568b7029b21b718938e0747efe11f0062a35
favourachara07 <acharafavour01@gmail.com>
2023-10-05 23:20:08 +0100

    updates

--- tut_port.js
+++ tut_port.js
@@ -1,35 +1,34 @@
 /*n=>n
     let itemClass=this.parentNode.getElementsByClassName*/
 // // opening and closing services
-const navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggles'),
-    navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggles"),
+  navClose = document.getElementById("nav-close");
 
 if (navToggle) {
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu');
-        console.log('Menu opened'); // Optional logging
-    });
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+    console.log("Menu opened"); // Optional logging
+  });
 }
 
 if (navClose) {
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu');
-    });
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
-
-const navLink=document.querySelectorAll('.nav_link')
-function linkAction(){
-    const navMenu= document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
+const navLink = document.querySelectorAll(".nav_link");
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n=>n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 // opening and closing skills
 const skillsContent = document.getElementsByClassName("skills_content"),
   skillsHeader = document.querySelectorAll(".skills_header");
-function togggleSkills(){
+function togggleSkills() {
   //dont understand
   let itemClass = this.parentNode.className;
 
@@ -39,30 +38,55 @@ function togggleSkills(){
   if (itemClass === "skills_content skills_close") {
     this.parentNode.className = "skills_content skills_open";
   }
-};
+}
 skillsHeader.forEach((el) => {
   el.addEventListener("click", togggleSkills);
 });
 
 //basic understanding but still refer to chatgpt
 // opening and closing services
-const modalViews=document.querySelectorAll('.services_modal'),
-  modalBtns=document.querySelectorAll('.services_button'),
-  modalClose=document.querySelectorAll('.services_modal-close')
-let modal=function(modalClick){
-  modalViews[modalClick].classList.add('active-modal')
-}
-modalBtns.forEach((modalBtn,i)=>{
-  modalBtn.addEventListener('click',()=>{
-    modal(i)
-  })
-})
-modalClose.forEach((modalClose)=>{
-  modalClose.addEventListener('click', ()=>{
-    modalViews.forEach((modalView)=>{
-      modalView.classList.remove('active-modal')
-    })
-  })
-})
-alert('not responsive yet!')
+const modalViews = document.querySelectorAll(".services_modal"),
+  modalBtns = document.querySelectorAll(".services_button"),
+  modalClose = document.querySelectorAll(".services_modal-close");
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+modalClose.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+    });
+  });
+});
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "fa-solid fa-sun";
 
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  document.body.classList.contains(iconTheme) ? "fa-solid fa-moon" : "fa-solid fa-sun";
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedTheme === "fa-solid fa-moon" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
